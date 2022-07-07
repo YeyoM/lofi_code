cc408e15fdecb6d2b1af06c520466d392f3f5a39
YeyoM <yeyomoreno2003@hotmail.com>
2022-07-06 21:09:24 -0500

    integrated react-terminal and added my commands

--- components/terminal/terminal.jsx
+++ components/terminal/terminal.jsx
@@ -1,9 +1,39 @@
 import classes from './terminal.module.css'
+import { ReactTerminal } from 'react-terminal'
+import { TerminalContextProvider } from 'react-terminal'
+
+const commands = {
+  hello: () => console.log("hello"),
+  svolume: (volume) => console.log("volume set to " + volume),
+  nextsong: () => console.log("next song"),
+  prevsong: () => console.log("previous song"),
+  play: () => console.log("play"),
+  pause: () => console.log("pause"),
+  help: (
+    <span>
+      hello                   - say hello <br/>
+      svolume &lt;VOLUME&gt;   - set volume to &lt;volume&gt <br/>
+      nextsong                - play next song <br/>
+      prevsong                - play previous song <br/>    
+      play                    - play song <br/>
+      pause                   - pause song <br/>
+      help                    - show this message <br/>    
+      clear                   - clear screen      
+    </span>
+  )
+}
 
 export default function Terminal() {
   return (
-    <div className={classes.terminal}>
-      <p>Terminal</p>
-    </div>
+    <TerminalContextProvider>
+      <div className={classes.terminal}>
+      <ReactTerminal
+        theme="dracula"
+        commands={commands}
+        showControlButtons={false}
+        showControlBar={false}
+      />
+      </div>
+    </TerminalContextProvider>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import '../styles/styles.css'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
