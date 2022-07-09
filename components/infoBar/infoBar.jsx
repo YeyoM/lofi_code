f834e54cc400d7990e26d54525ecf5568409db42
YeyoM <yeyomoreno2003@hotmail.com>
2022-07-08 21:39:43 -0500

    fixed bugs, weather correct in infobar

--- components/infoBar/infoBar.jsx
+++ components/infoBar/infoBar.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import classes from './infoBar.module.css'
 
+import useGeolocation from '../../hooks/useGeolocation/useGeolocation'
+
 function getDate() {
   const date = new Date()
   const month = date.getMonth() + 1
@@ -9,17 +11,40 @@ function getDate() {
   return `${month}/${day}/${year}`
 }
 
+async function getWeather(latitude, longitude) {
+  const response = await fetch('http://localhost:3000/api/getWeather', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      },
+    body: JSON.stringify({
+      latitude: latitude,
+      longitude: longitude
+    })
+  })
+  const data = await response.json()
+  return data
+}
+
 export default function InfoBar() {
 
   const [date, setDate] = useState()
   const [time, setTime] = useState()
+  const [weather, setWeather] = useState()
+
+  const location = useGeolocation()
 
   useEffect(() => {
     setDate(getDate())
+    getWeather(location.coordinates.latitude, location.coordinates.longitude).then(data => {
+      setWeather(data.weather[0].main)
+    }).catch(error => {
+      console.log(error)
+    })
     setInterval(() => {
       setTime(new Date().toLocaleTimeString())
     }, 1000)
-  }, [])
+  }, [location])
 
   return (
     <div className={classes.infoBar}>
@@ -33,7 +58,7 @@ export default function InfoBar() {
         {date}
       </div>
       <div className={classes.weather}>
-        Sunny
+        {weather}
       </div>
     </div>  
   )
