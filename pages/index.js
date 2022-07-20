c47a4fd27e176e512dc3251378bc92a0e51e927d
YeyoM <yeyomoreno2003@hotmail.com>
2022-07-19 23:50:22 -0500

    metadata with next/head

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import React, { Fragment } from 'react'
+import Head from 'next/head'
+
 import Main from "../components/main/main"
 
 export default function Home() {
   return (
-    <Main />
+    <Fragment>
+      <Head>
+        <title>Lofi Terminal</title>
+        <link rel="icon" href="/icon.svg" />
+        <meta 
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <meta 
+          name="description" 
+          content="The Lofi Terminal, a music player that will make any programmer's life easier." 
+        />
+      </Head>
+      <Main />
+    </Fragment>
   )
 }
