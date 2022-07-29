7ca7ebb92a1c1eb7cb1ad7bb89ebe9f8b013f3d4
YeyoM <yeyomoreno2003@hotmail.com>
2022-07-29 11:34:59 -0500

    google site verification

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,10 @@ export default function Home() {
           name="description" 
           content="The Lofi Terminal, a music player that will make any programmer's life easier." 
         />
+        <meta 
+          name="google-site-verification" 
+          content="JOI4AGYf-0LIW42s2z_eHwSuUpBzQxF4hxZMoCveilw" 
+        />
       </Head>
       <Main />
     </Fragment>
