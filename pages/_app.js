bc8ffc8ddd7ebaddde3aee4878e050589afb3d1c
YeyoM <yeyomoreno2003@hotmail.com>
2023-02-03 21:53:25 -0600

    fix: :bug: Fixed bug involved with next head in _app.js and _document.js
    FIxed bug
    
    No breaking changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '../styles/styles.css'
-import { Head } from 'next/document'
+import Head from 'next/head'
 import { SongsContextProvider } from '../components/context/songsContext'
 
 function MyApp ({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/icon.svg" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.svg" />
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
       </Head>
       <SongsContextProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,13 @@
-import Document, { Html, Main, NextScript } from 'next/document'
+import Document, { Html, Main, Head, NextScript } from 'next/document'
 import React from 'react'
 class myDocument extends Document {
   render () {
     return <Html lang='en'>
+      <Head>
+        <link rel="icon" href="/icon.svg" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+      </ Head>
       <body>
         <Main />
         <NextScript />
