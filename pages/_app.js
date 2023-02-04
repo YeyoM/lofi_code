d47939e801a1f2bca00655bcc4e7157ef43b4e06
YeyoM <yeyomoreno2003@hotmail.com>
2023-02-03 21:49:41 -0600

    refactor: :art: Moved Head Element from _document.js to _app.js
    Moved Head Element from _document.js to _app.js
    
    No breaking changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import '../styles/styles.css'
+import { Head } from 'next/document'
 import { SongsContextProvider } from '../components/context/songsContext'
 
 function MyApp ({ Component, pageProps }) {
   return (
-    <SongsContextProvider>
-      <Component {...pageProps} />
-    </ SongsContextProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/icon.svg" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
+      </Head>
+      <SongsContextProvider>
+        <Component {...pageProps} />
+      </ SongsContextProvider>
+    </>
   )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,8 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Main, NextScript } from 'next/document'
 import React from 'react'
 class myDocument extends Document {
   render () {
     return <Html lang='en'>
-      <Head>
-        <link rel="icon" href="/icon.svg" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.svg" />
-        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
-      </Head>
       <body>
         <Main />
         <NextScript />
